a6a32c0245174e66cf45561d705c36f762fe21d2
陈旭斌 <xbchen@travelsky.com>
2021-02-25 16:01:59 +0800

    helper.js 封装checkDir --用于检测临时目录，如果目录不存在时自动创建目录

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,11 +1,13 @@
 const moment = require('moment');
+const path = require('path')
+const fsPromise = require('fs/promises')
 // 格式化时间
 exports.formatTime = time => moment(time)
   .format('YYYY-MM-DD HH:mm:ss');
 // 处理成功响应
-exports.success = ({ ctx, res = null, msg = '请求成功' }) => {
+exports.success = ({ctx, res = null, msg = '请求成功'}) => {
   // 请求日志
-  const { method } = ctx.request;
+  const {method} = ctx.request;
   if (method === 'GET') {
     ctx.logger.info('request.query', ctx.request.query);
   } else {
@@ -25,6 +27,16 @@ exports.success = ({ ctx, res = null, msg = '请求成功' }) => {
   };
 };
 // 有效时间为30分钟
-exports.getToken = function(options) {
-  return this.app.jwt.sign(options, this.app.config.jwt.secret, { expiresIn: '1800s' });
+exports.getToken = function (options) {
+  return this.app.jwt.sign(options, this.app.config.jwt.secret, {expiresIn: '1800s'});
 };
+exports.checkDir = async function (url) {
+  const absUrl = path.join(this.app.baseDir, url)
+  try {
+    const res = await fsPromise.stat(absUrl)
+    // console.log('结果：', res)
+  } catch (e) {
+    // console.log('错误:', e)
+    await fsPromise.mkdir(absUrl)
+  }
+}
--- app/service/upload.js
+++ app/service/upload.js
@@ -1,27 +1,29 @@
 'use strict';
-const { Service } = require('egg');
+const {Service} = require('egg');
 const fs = require('fs');
 const path = require('path');
 const pump = require('mz-modules/pump');
+const util = require('util')
 
 class UserService extends Service {
   // 上传图片
   async uploadImg() {
     // 添加用户信息
-    const { ctx } = this;
-    const parts = ctx.multipart({ autoFields: true });
+    const {ctx} = this;
+    const parts = ctx.multipart({autoFields: true});
     const urls = [];
     let stream;
     while ((stream = await parts()) != null) {
-      const fileType = stream.mimeType.split('/')[ 1 ];
+      const fileType = stream.mimeType.split('/')[1];
       const filename = Date.now() + '.' + fileType || stream.filename.toLowerCase();
+      await ctx.helper.checkDir('app/public/temporary')
       const target = path.join(this.config.baseDir, 'app/public/temporary', filename);
       urls.push(`/public/temporary/${filename}`);
       const writeStream = fs.createWriteStream(target);
       await pump(stream, writeStream);
     }
-    console.log(urls);
-    return ctx.helper.success({ ctx, res: urls });
+    // console.log(urls);
+    return ctx.helper.success({ctx, res: urls});
   }
 }
 
